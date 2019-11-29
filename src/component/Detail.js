4b52a0a257bf99bde796869a714f299655efab7a
Robbert van 't Westende <robbert@totaldesign.com>
2019-11-30 00:39:26 +0100

    Update styles.

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import moment from 'moment';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import Slider from './Slider';
 
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+  }
+});
+
 const Detail = props => {
+  const classes = useStyles();
   const date = moment(props.date);
+
   return (
-    <div>
-      <Slider images={props.images} />
-      <h1>{date.format('dddd D MMMM YYYY')}</h1>
-      <p>{props.description}</p>
-    </div>
+    <Card className={classes.card}>
+      <CardContent>
+        <Slider images={props.images} />
+        <Typography variant="h5" component="h2">{date.format('dddd D MMMM YYYY')}</Typography>
+        <Typography variant="body1" component="p">{props.description}</Typography>
+      </CardContent>
+    </Card>
   );
 };
 
--- src/component/Slider.js
+++ src/component/Slider.js
@@ -4,7 +4,7 @@ import SwipeableViews from 'react-swipeable-views';
 const Slider = props => {
   if (props.images) {
     return (
-      <SwipeableViews containerStyle={{ height: '250px', 'textAlign': 'center' }} enableMouseEvents>
+      <SwipeableViews containerStyle={{ height: '250px', textAlign: 'center', marginBottom: '1rem' }} enableMouseEvents>
         {props.images.map(image => {
           return (
             <img src={image.small} key={image.ref} style={{ height: '100%' }}/>
